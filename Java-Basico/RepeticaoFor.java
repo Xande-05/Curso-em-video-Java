e0d4b12acafef36e05181f1f7f7cb2336215f775
ALEXANDRE LICIO DA SILVA MORAIS <xandylicio@gmail.com>
2025-09-13 23:20:36 -0300

    Commit

---
+++ Java-Basico/RepeticaoFor.java
@@ -0,0 +1,12 @@
+package repeticao;
+
+public class RepeticaoFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int cc = 0; cc <= 100; cc += 10) {
+			System.out.println(cc);
+		}
+	}
+
+}
