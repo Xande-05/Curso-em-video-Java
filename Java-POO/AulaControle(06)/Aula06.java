c37200991a03f48fec8ef947f97daf0006ff708d
ALEXANDRE LICIO DA SILVA MORAIS <xandylicio@gmail.com>
2025-09-16 11:36:51 -0300

    Aula06-ExercicioBanco

---
+++ Java-POO/AulaControle(06)/Aula06.java
@@ -0,0 +1,15 @@
+package aula06;
+
+public class Aula06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Controle controle = new Controle();
+		controle.ligar();
+		controle.maisVolume();
+		controle.play();
+		controle.abrirMenu();
+		controle.fecharMenu();
+	}
+
+}
