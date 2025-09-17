9e5d60cabb655b682cf0c004272b82974311a5bc
ALEXANDRE LICIO DA SILVA MORAIS <xandylicio@gmail.com>
2025-09-17 12:47:32 -0300

    Aula10ExercicioPessoa

---
+++ Java-POO/Aula10Aluno/ProjetoPessoa.java
@@ -0,0 +1,28 @@
+package aula10;
+
+public class ProjetoPessoa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pessoa p1 = new Pessoa();
+		Aluno p2 = new Aluno();
+		Professor p3 = new Professor();
+		Funcionario p4 = new Funcionario();
+		
+		p1.setNome("Pedro");
+		p2.setNome("Maria");
+		p3.setNome("Claudio");
+		p4.setNome("Fabiana");
+		
+		p1.setSexo("M");
+		p2.setSexo("F");
+		p3.setSexo("M");
+		p4.setSexo("F");
+		
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		System.out.println(p3.toString());
+		System.out.println(p4.toString());
+	}
+
+}
